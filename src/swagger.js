d40c8a9fbee9a54bb6ac035a63a9510424be2003
Tomasbri889 <tomasbri889@gmail.com>
2025-10-26 00:05:43 -0400

    dandole

--- src/swagger.js
+++ src/swagger.js
@@ -28,7 +28,8 @@ const swaggerDefinition = {
 const options = {
   swaggerDefinition,
   // 👇 Asegúrate de incluir todas tus rutas y el app principal
-   apis: ['./**/*.js'],
+   apis: ['./routes/*.js', './app.js']
+
 
 };
 
